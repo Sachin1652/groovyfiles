134ca2c302d6ef90ac2baad0836f0db7b20893ad
Sachin <92563151+Sachin1652@users.noreply.github.com>
2026-01-13 22:50:24 +0530

    Add runPlaybook function for Ansible execution

---
+++ vars/runPlaybook.groovy
@@ -0,0 +1,16 @@
+def call(Map cfg) {
+
+    withCredentials([[
+        $class: 'AmazonWebServicesCredentialsBinding',
+        credentialsId: 'aws-cred'
+    ]]) {
+
+        dir(cfg.CODE_BASE_PATH) {
+            sh """
+              ansible-playbook ${cfg.ANSIBLE.PLAYBOOK} \
+              -i ${cfg.ANSIBLE.INVENTORY} \
+              -f ${cfg.ANSIBLE.FORKS}
+            """
+        }
+    }
+}
